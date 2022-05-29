ddc51fa4946d83a6b0794c1f539388c57dd87d1e
zengqingrun <1972580211@qq.com>
2022-05-29 15:29:04 +0800

    5.29

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,16 +5,16 @@
              :rules="loginRules"
              ref="loginForm"
              label-position="left"
-             label-width="0px"
+             label-width="0rem"
              class="card-box login-form">
       <p class="title"
-         style="font-size:28px;color:#000">欢迎登录·邓州市第一高级中学 </p>
+         style="font-size:28rem;color:#000">欢迎登录·邓州市第一高级中学</p>
       <h2 class="title"
-          style="font-size:38px;color:#0053b5">班级管理系统</h2>
-      <p style="text-align:center">{{loginType===1?'密码登录':'验证码登录'}}</p>
+          style="font-size:38rem;color:#0053b5">文印室管理系统</h2>
+      <p style="text-align:center;font-size: 16rem;margin-top: 30rem;">{{loginType===1?'密码登录':'验证码登录'}}</p>
       <div v-if="loginType===1">
         <el-form-item prop="email">
-          <div>账号</div>
+          <div style="font-size: 16rem;">账号</div>
           <el-input name="email"
                     type="text"
                     v-model="loginForm.email"
@@ -22,17 +22,18 @@
                     placeholder="请输入您的账号/手机号"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <div>密码</div>
-          <el-input name="password"
+          <div style="font-size: 16rem;">密码</div>
+          <el-input
+                    name="password"
                     type="password"
                     @keyup.enter.native="handleLogin"
                     v-model="loginForm.password"
                     autoComplete="off"
                     placeholder="请输入密码"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary"
-                     style="width:100%;"
+        <el-form-item style="margin-top: 50rem;">
+          <el-button  type="primary"
+                     style="width:100%"
                      :loading="loading"
                      @click.native.prevent="handleLogin">
             登录
@@ -40,7 +41,7 @@
         </el-form-item>
         <el-form-item>
           <el-button type="text"
-                     style="width:100%;color:#000;border: 1px solid #333333;"
+                     style="width:100%;color:#000;border: 1rem solid #333333;"
                      :loading="loading"
                      @click.native.prevent="loginType=2">
             验证码登录
@@ -49,7 +50,7 @@
       </div>
       <div v-if="loginType===2">
         <el-form-item prop="email">
-          <div>手机号</div>
+          <div style="font-size: 16rem;">手机号</div>
           <el-input name="email"
                     type="text"
                     v-model="loginForm.email"
@@ -57,7 +58,7 @@
                     placeholder="请输入您的账号/手机号"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <div>验证码</div>
+          <div style="font-size: 16rem;">验证码</div>
           <el-input name="password"
                     type="password"
                     @keyup.enter.native="handleLogin"
@@ -65,7 +66,7 @@
                     autoComplete="off"
                     placeholder="请输入密码"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item style="margin-top: 50rem;">
           <el-button type="primary"
                      style="width:100%;"
                      :loading="loading"
@@ -75,7 +76,7 @@
         </el-form-item>
         <el-form-item>
           <el-button type="text"
-                     style="width:100%;color:#000;border: 1px solid #333333;"
+                     style="width:100%;color:#000;border: 1rem solid #333333;"
                      :loading="loading"
                      @click.native.prevent="loginType=1">
             密码登录
@@ -170,7 +171,7 @@ export default {
             this.loading = false;
 
             console.log('登陆成功即将跳转--------')
-            this.$router.push({ path: '/classManage/permissionsManage' });
+            this.$router.push({ path: '/class-manage/permissionsManage' });
 
 
             // this.showDialog = true;
@@ -215,50 +216,59 @@ export default {
 <style rel="stylesheet/scss" lang="scss">
 @import "src/assets/css/mixin.scss";
 .tips {
-  font-size: 14px;
+  font-size: 14rem;
   color: #fff;
-  margin-bottom: 5px;
+  margin-bottom: 5rem;
 }
 .login-container {
+
   @include relative;
   height: 100vh;
   /*background-color: #2d3a4b;*/
 
   background: url("../../assets/img/login_bg.png") no-repeat;
   background-size: cover;
+  // background-size: 100% auto;
+  background-position: center center;
+  // background-attachment:fixed;
 
   input:-webkit-autofill {
-    -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
+    -webkit-box-shadow: 0 0 0rem 1000rem #293444 inset !important;
     -webkit-text-fill-color: #fff !important;
+    background-position: center 0;
   }
   input {
     background: transparent;
-    border: 0px;
+    border: 0rem;
     -webkit-appearance: none;
-    border-radius: 0px;
-    padding: 12px 5px 12px 15px;
+    border-radius: 0rem;
+    padding: 0rem;
     color: #333;
-    height: 47px;
+    height: 30rem;
   }
   .el-input {
     display: inline-block;
-    height: 47px;
+    height: 30rem;
     width: 85%;
+    font-size: 14rem;
+  }
+  .el-form-item__content{
+    line-height: 30rem;
   }
   .svg-container {
-    padding: 6px 5px 6px 15px;
-    width: 40px;
+    padding: 6rem 5rem 6rem 15rem;
+    width: 40rem;
     float: left;
     text-align: center;
     color: #889aa4;
-    font-size: 20px;
+    font-size: 20rem;
   }
 
   .title {
-    font-size: 26px;
+    font-size: 26rem;
     font-weight: 400;
     color: #eeeeee;
-    margin: 0px auto 10px auto;
+    margin: 0rem auto 10rem auto;
     text-align: center;
     font-weight: bold;
   }
@@ -267,18 +277,19 @@ export default {
     position: absolute;
     // left: 0;
     top: 15%;
-    right: 30%;
-    // width: 400px;
-    padding: 35px 35px 15px 35px;
-    margin: 80px auto 0;
+    right: 24%;
+    // width: 400rem;
+        width: 450rem;
+    padding: 35rem 35rem 15rem 35rem;
+    margin: 40rem auto 0;
     // margin: 0 auto;
   }
 
   .el-form-item {
-    // border: 1px solid rgba(255, 255, 255, 0.1);
+    // border: 1rem solid rgba(255, 255, 255, 0.1);
     // background: rgba(0, 0, 0, 0.1);
-    // border-radius: 5px;
-    border-bottom: 1px solid #dddddd;
+    // border-radius: 5rem;
+    border-bottom: 1rem solid #dddddd;
     color: #333;
   }
 
